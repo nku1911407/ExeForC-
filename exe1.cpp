8197afa239115d4cf9dc307a2fbcd234703af69b
nku1911407 <duanguoha0@163.com>
2023-03-31 14:39:24 +0800

    helo

---
+++ exe1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+// define a function sum(int, int)
+int sum(int a, int b)
+{
+    return a+b;
+}
+
+int main()
+{
+    cout<<"Hello World";
+    return 0;
+}
+
